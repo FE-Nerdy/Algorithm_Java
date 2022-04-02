d56ee300861cd258066a55b05caddf6ca21c4081
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2022-04-02 23:42:49 +0900

    Input/Output_and_Arithmetic_Operation

---
+++ Algorithm_Step1/src/A_plus_B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class A_plus_B {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a+b);
+	}
+}
