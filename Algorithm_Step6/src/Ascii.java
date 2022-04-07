5c025017b3dd064648b2377e561f633dfd3b8327
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2022-04-07 18:37:00 +0900

    String

---
+++ Algorithm_Step6/src/Ascii.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Ascii {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int ch = sc.next().charAt(0);
+		System.out.println(ch);
+		
+		sc.close();
+	}
+}
