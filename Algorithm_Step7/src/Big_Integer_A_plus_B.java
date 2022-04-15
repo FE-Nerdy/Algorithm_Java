8b5ab41ab7bacaca403725744196a4dc13906b42
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2022-04-16 04:21:01 +0900

    Basic_Math1

---
+++ Algorithm_Step7/src/Big_Integer_A_plus_B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.math.BigInteger;
+
+public class Big_Integer_A_plus_B {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger A = new BigInteger(sc.next());
+		BigInteger B = new BigInteger(sc.next());
+
+		A = A.add(B);
+		
+		System.out.println(A.toString());
+	}
+}
