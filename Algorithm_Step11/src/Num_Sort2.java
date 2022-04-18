7c6ddd4fd282f5277ad268a75c76ef2c3be5730d
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2022-04-18 16:42:51 +0900

    Sort

---
+++ Algorithm_Step11/src/Num_Sort2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Num_Sort2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int N = sc.nextInt();
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		for(int i = 0; i < N; i++) {
+			list.add(sc.nextInt());
+		}
+		
+		Collections.sort(list);
+		
+		for(int value : list) {
+			sb.append(value).append('\n');
+		}
+		System.out.println(sb);
+
+	}
+
+}
