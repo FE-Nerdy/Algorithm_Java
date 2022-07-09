b5098213b122796f29f914c268daaeabcd4cc620
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2022-07-09 23:06:22 +0900

    DFS, BFS

---
+++ Inflearn_DFS_BFS/src/Puppy.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Puppy{
+	static int answer=Integer.MIN_VALUE, c, n;
+	public void DFS(int L, int sum, int[] arr){
+		if(sum>c) return;
+		if(L==n){
+			answer=Math.max(answer, sum);
+		}
+		else{
+			DFS(L+1, sum+arr[L], arr);
+			DFS(L+1, sum, arr);
+		}	
+	}
+
+	public static void main(String[] args){
+		Puppy P = new Puppy();
+		Scanner sc = new Scanner(System.in);
+		c=sc.nextInt();
+		n=sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0; i<n; i++){
+			arr[i]=sc.nextInt();
+		}
+		P.DFS(0, 0, arr);
+		System.out.println(answer);
+		sc.close();
+	}
+}
