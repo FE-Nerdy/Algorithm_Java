e9bb8747e3907b91dbf89e5f58a2f6aae408330e
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2022-07-11 23:20:20 +0900

    DFS, BFS

---
+++ Inflearn_DFS_BFS/src/CoinChange.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class CoinChange{
+	static int n, m, answer=Integer.MAX_VALUE;
+	public void DFS(int L, int sum, Integer[] arr){
+		if(sum>m) return;
+		if(L>=answer) return;
+		if(sum==m){
+			answer=Math.min(answer, L);
+		}
+		else{
+			for(int i=0; i<n; i++){
+				DFS(L+1, sum+arr[i], arr);
+			}
+		}	
+	}
+	public static void main(String[] args){
+		CoinChange CC = new CoinChange();
+		Scanner sc = new Scanner(System.in);
+		n=sc.nextInt();
+		Integer[] arr=new Integer[n];
+		for(int i=0; i<n; i++) arr[i]=sc.nextInt();
+		Arrays.sort(arr, Collections.reverseOrder());
+		m=sc.nextInt();
+		CC.DFS(0, 0, arr);
+		System.out.println(answer);
+		sc.close();
+	}
+}
---
+++ Inflearn_DFS_BFS/src/Permutation.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Permutation{
+	static int[] pm, ch, arr;
+	static int n, m;
+	public void DFS(int L){
+		if(L==m){
+			for(int x : pm) System.out.print(x+" ");
+			System.out.println();
+		}
+		else{
+			for(int i=0; i<n; i++){
+				if(ch[i]==0){
+					ch[i]=1;
+					pm[L]=arr[i];
+					DFS(L+1);
+					ch[i]=0;
+				}
+			}
+		}
+	}
+	public static void main(String[] args){
+		Permutation P = new Permutation();
+		Scanner sc = new Scanner(System.in);
+		n=sc.nextInt();
+		m=sc.nextInt();
+		arr=new int[n];
+		for(int i=0; i<n; i++) arr[i]=sc.nextInt();
+		ch=new int[n];
+		pm=new int[m];
+		P.DFS(0);
+		sc.close();
+	}
+}
