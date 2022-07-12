d4d8ca3095c1949da1c67157fe2971200db4d401
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2022-07-12 21:59:38 +0900

    DFS, BFS

---
+++ Inflearn_DFS_BFS/src/Combination.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Combination{
+	int[][] dy=new int[35][35];
+	public int DFS(int n, int r){
+		if(dy[n][r]>0) return dy[n][r];
+		if(n==r || r==0) return 1;
+		else return dy[n][r]=DFS(n-1, r-1)+DFS(n-1, r);
+	}
+	public static void main(String[] args){
+		Combination C = new Combination();
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int r=sc.nextInt();
+		System.out.println(C.DFS(n, r));
+		sc.close();
+	}
+}
