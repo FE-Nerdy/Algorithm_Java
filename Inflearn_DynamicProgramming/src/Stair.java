0102df4b327d09f681af31093022028c719e6cea
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2022-07-22 23:36:46 +0900

    DynamicProgramming

---
+++ Inflearn_DynamicProgramming/src/Stair.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Stair{
+	static int[] dy;
+	public int solution(int n){
+		dy[1]=1;
+		dy[2]=2;
+		for(int i=3; i<=n; i++) dy[i]=dy[i-2]+dy[i-1];
+		return dy[n];
+	}
+
+	public static void main(String[] args){
+		Stair S = new Stair();
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		dy=new int[n+1];
+		System.out.print(S.solution(n));
+		sc.close();
+	}
+}
